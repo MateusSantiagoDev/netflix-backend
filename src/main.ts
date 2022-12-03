3397eafb35a7a1f4e1d4d495119fa78fca90e808
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-03 16:31:41 -0300

    chore: implemented swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Netflix')
+    .setVersion('Aplicação de filmes e series')
+    .setVersion('1.0.0')
+    .addTag('Status')
+    .addTag('Movies')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
